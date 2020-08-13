c487fb1a2e09a5b914077a7249c93080cd0cc7b2
rajmohan6268 <48435037+rajmohan6268@users.noreply.github.com>
2020-08-14 02:25:00 +0530

    (feat):text box and form included

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,36 +12,19 @@
           </a>
         </div>
 
-        <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-white px-2">
-          <span class="relative w-full">
-            <input
-              type="search"
-              placeholder="Search"
-              class="w-full bg-gray-800 text-sm text-white transition border border-transparent focus:outline-none focus:border-gray-700 rounded py-1 px-2 pl-10 appearance-none leading-normal"
-            />
-            <div class="absolute search-icon" style="top: .5rem; left: .8rem;">
-              <svg
-                class="fill-current pointer-events-none text-white w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"
-                />
-              </svg>
-            </div>
-          </span>
-        </div>
-        <div class="flex w-full pt-2 content-center justify-between md:w-1/3 md:justify-end">
-          <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
+        <div class="flex w-full pt-2 content-end justify-between md:w-1/3 md:justify-end">
+          <ul class="list-reset flex justify-between flex-1 md:flex-none items-end">
             <li class="flex-1 md:flex-none md:mr-3">
-              <a class="inline-block py-2 px-4 text-white no-underline" href="/test">test</a>
+              <a
+                class="inline-block py-2 px-4 text-white no-underline"
+                href="/test"
+              >test</a>
             </li>
             <li class="flex-1 md:flex-none md:mr-3">
               <a
                 class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
-                href="#"
-              >link</a>
+                href="/try"
+              >try</a>
             </li>
             <li class="flex-1 md:flex-none md:mr-3">
               <div class="relative inline-block"></div>
@@ -52,25 +35,17 @@
     </nav>
 
     <div class="flex flex-col md:flex-row">
-      <div
-        class="bg-gray-900 shadow-lg h-16 fixed bottom-0 mt-12 md:relative md:h-screen z-10 w-full md:w-48"
-      >
-        <div
-          class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between"
-        >
-          <ul
-            class="list-reset flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left"
-          >
+      <div class="bg-gray-900 shadow-lg h-16 fixed bottom-0 mt-12 md:relative md:h-screen z-10 w-full md:w-48">
+        <div class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
+          <ul class="list-reset flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left">
             <li class="mr-3 flex-1">
               <a
-                href="#"
+                href="/"
                 class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500"
               >
                 <i class="fas fa-tasks pr-0 md:pr-3"></i>
-                <span
-                  class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block"
-                >Tasks</span>
-              </a>
+                <span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Tasks</span>
+                </a>
             </li>
             <li class="mr-3 flex-1">
               <a
@@ -78,10 +53,8 @@
                 class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500"
               >
                 <i class="fa fa-envelope pr-0 md:pr-3"></i>
-                <span
-                  class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block"
-                >crud</span>
-              </a>
+                <span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">crud</span>
+                </a>
             </li>
 
             <li class="mr-3 flex-1">
@@ -90,10 +63,8 @@
                 class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500"
               >
                 <i class="fa fa-envelope pr-0 md:pr-3"></i>
-                <span
-                  class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block"
-                >get</span>
-              </a>
+                <span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">get</span>
+                </a>
             </li>
             <li class="mr-3 flex-1">
               <a
@@ -101,10 +72,8 @@
                 class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-gray-800 border-b-2 hover:border-blue-600"
               >
                 <i class="fas fa-chart-area pr-0 md:pr-3 text-blue-600"></i>
-                <span
-                  class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block"
-                >post</span>
-              </a>
+                <span class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">post</span>
+                </a>
             </li>
             <li class="mr-3 flex-1">
               <a
@@ -112,10 +81,8 @@
                 class="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline border-b-2 border-gray-800 hover:border-red-500"
               >
                 <i class="fa fa-wallet pr-0 md:pr-3"></i>
-                <span
-                  class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block"
-                >put</span>
-              </a>
+                <span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">put</span>
+                </a>
             </li>
             <li class="mr-3 flex-1">
               <a
@@ -123,10 +90,8 @@
                 class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500"
               >
                 <i class="fa fa-envelope pr-0 md:pr-3"></i>
-                <span
-                  class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block"
-                >delete</span>
-              </a>
+                <span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">delete</span>
+                </a>
             </li>
           </ul>
         </div>
--- pages/test.vue
+++ pages/test.vue
@@ -1,7 +1,112 @@
 <template>
+  <div>
+    <div class="bg-blue-800 p-2 shadow text-xl text-white">
+      <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-white px-2">
+        <span class="relative w-full">
+          <input
+            v-model="title"
+            type="search"
+            placeholder="Search by Title"
+            class="w-full bg-gray-800 text-sm text-white transition border border-transparent focus:outline-none focus:border-gray-700 rounded py-1 px-2 pl-10 appearance-none leading-normal"
+          />
+          <div
+            class="absolute search-icon"
+            style="top: .5rem; left: .8rem;"
+          >
+            <svg
+              class="fill-current pointer-events-none text-white w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+              </svg>
+      </div>
+
+      </span>
+
+    </div>
+  </div>
   <div class="flex flex-wrap -mx-4 -mb-4 md:mb-0">
+
     <div class="w-full md:w-1/2 px-4 mb-4 md:mb-0">
-      <div v-if="!currentPost" class="col-md-8">
+      <div
+        v-if="!currentPost"
+        class="w-full max-w-xs"
+      >
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div v-if="!submitted">
+            <div class="mb-4">
+              <span class="font-bold text-2xl text-gray-300">Add new post</span>
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="title"
+              >
+                Title
+                </label>
+                <input
+                  id="post.title"
+                  v-model="post.title"
+                  type="text"
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  required
+                  name="title"
+                  placeholder="Enter Title here"
+                >
+            </div>
+            <div class="mb-4">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="body"
+              >
+                body
+                </label>
+                <input
+                  id="body"
+                  v-model="post.body"
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  required
+                  name="tender_refno"
+                  placeholder="Enter post body here"
+                >
+            </div>
+            <div class="mb-4">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2"
+                for="userId"
+              >
+                UserId
+                </label>
+                <input
+                  id="userId"
+                  v-model="post.userId"
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  required
+                  name="userId"
+                  placeholder="Enter userId"
+                >
+            </div>
+            <div class="flex items-center justify-between">
+              <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                @click="savePost"
+                type="button"
+              >
+                Submit (create post)
+                </button>
+
+            </div>
+          </div>
+          <div v-else>
+            <h4>You submitted successfully!</h4>
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              @click="newPost"
+            >Add new</button>
+          </div>
+        </form>
+
+    </div>
+    <!-- <div v-if="!currentPost" class="col-md-8">
         <div class="submit-form">
           <div v-if="!submitted">
             <h1 class>Add new post</h1>
@@ -50,105 +155,138 @@
             <button class="btn btn-success" @click="newPost">Add</button>
           </div>
         </div>
+      </div> -->
+    <div class="row-md-6">
+      <div v-if="currentPost">
+        <h3>
+          <b>total posts : {{ posts.length }}</b>
+        </h3>
       </div>
-      <div class="row-md-6">
-        <div v-if="currentPost">
-          <h3>
-            <b>total posts : {{ posts.length }}</b>
-          </h3>
-        </div>
-        <div class="input-group mb-3">
-          <input v-model="title" type="text" class="form-control" placeholder="Search by Title" />
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button" @click="searchTitle">Search</button>
-          </div>
-        </div>
-        <h4>
-          <b>post List</b>
-        </h4>
-        <ul class="list-group">
-          <li
-            v-for="(post, index) in posts"
-            :key="index"
-            class="list-group-item"
-            :class="{ active: index == currentIndex }"
-            @click="setActivePost(post, index)"
-          >{{ post.title }}</li>
-        </ul>
 
-        <button class="m-3 btn btn-sm btn-danger" @click="removeAllposts()">Remove All</button>
-      </div>
-      <div class="col-md-6">
-        <div v-if="currentPost" class="edit-form">
-          <h4>
-            <b>Post detail</b>
-          </h4>
-          <form>
-            <div class="form-group">
-              <label for="title">
-                <B>post title</B>
-              </label>
-              <input id="title" v-model="currentPost.title" type="text" class="form-control" />
-            </div>
+      <h4>
+        <b>post List</b>
+      </h4>
+      <ul class="list-group">
+        <li
+          v-for="(post, index) in posts"
+          :key="index"
+          class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          @click="setActivePost(post, index)"
+        >{{ post.title }}</li>
+      </ul>
 
-            <div class="form-group">
-              <label for="body">
-                <B>post body</B>
-              </label>
-              <input id="body" v-model="currentPost.body" class="form-control" />
-            </div>
-
-            <div class="form-group">
-              <label for="userId">user id</label>
-              <input id="userId" v-model="currentPost.userId" class="form-control" />
-            </div>
-          </form>
-
-          <button class="badge badge-danger mr-2" @click="deletePost">Delete</button>
-
-          <button type="submit" class="badge badge-success" @click="updatePost">Update</button>
-          <p>{{ message }}</p>
-        </div>
-
-        <div v-else>
-          <br />
-          <p>
-            <B>Please click on a Post...</B>
-          </p>
-        </div>
-      </div>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        @click="removeAllposts()"
+      >Remove All</button>
     </div>
-    <div class="w-full md:w-1/2 px-4 mb-4 md:mb-0">
-      <div class="flex flex-wrap">
-        <div v-for="post in posts" :key="post.id" class="w-full md:w-1/2 xl:w-1/3 p-3">
-          <div class="bg-blue-100 rounded-lg shadow-lg p-5">
-            <div class="flex flex-row items-center">
-              <div class="flex-1 text-right md:text-center">
-                <p class="font-bold text-blue-500">post:{{ post.id }}</p>
-                <h5 class="font-bold uppercase text-gray-600">
-                  <span class="font-extrabold text-xl flex-inline uppercase text-blue-600">Title :</span>
-                  {{ post.title }}
-                </h5>
-                <h3 class="font-bold text-2xl">
-                  <span
-                    class="font-extrabold text-xl flex-inline uppercase text-purple-600"
-                  >post body :</span>
-                  {{ post.body }}
-                  <span class="text-green-500">
-                    <i class="fas fa-caret-up">
-                      <p class="font-bold">user:{{ post.userId }}</p>
-                    </i>
-                  </span>
-                </h3>
-              </div>
-            </div>
-            <button
-              class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-            >edit post</button>
-          </div>
+
+    <div class="col-md-6">
+      <div
+        v-if="currentPost"
+        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div class="mb-4">
+          <b>Post detail</b>
         </div>
-      </div>
+        <form>
+          <div class="block text-gray-700 text-sm font-bold mb-2">
+            <label for="title">
+              <B>post title</B>
+            </label>
+            <input
+              id="title"
+              v-model="currentPost.title"
+              type="text"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+
+          <div class="block text-gray-700 text-sm font-bold mb-2">
+            <label for="body">
+              <B>post body</B>
+            </label>
+            <input
+              id="body"
+              v-model="currentPost.body"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+
+          <div class="block text-gray-700 text-sm font-bold mb-2">
+            <label for="userId">user id</label>
+            <input
+              id="userId"
+              v-model="currentPost.userId"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <table>
+            <th>
+              <Span class="font-bold text-blue-300">post id: </Span>
+            </th>
+
+            <th class="font-bold align-baseline">{{currentPost.id}}</th>
+
+          </table>
+
+        </form>
+
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          @click="deletePost"
+        >Delete</button>
+
+          <button
+            type="submit"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            @click="updatePost"
+          >Update</button>
+            <p>{{ message }}</p>
     </div>
+
+    <div v-else>
+      <br />
+      <p>
+        <B>Please click on a Post...</B>
+      </p>
+    </div>
+  </div>
+  </div>
+
+  <div class="w-full md:w-1/2 px-4 mb-4 md:mb-0">
+    <div class="flex flex-wrap">
+      <div
+        v-for="post in posts"
+        :key="post.id"
+        class="w-full p-3"
+      >
+        <div class="bg-blue-100 rounded-lg shadow-lg p-5">
+          <div class="flex flex-row items-center">
+            <div class="flex-1 text-right md:text-center">
+              <p class="font-bold text-blue-500">post:{{ post.id }}</p>
+              <h5 class="font-bold uppercase text-gray-600">
+                <span class="font-medium  flex-inline uppercase text-blue-600">Title :</span>
+                {{ post.title }}
+              </h5>
+              <h3 class="font-bold text-base">
+                <span class=" flex-inline uppercase text-purple-600">post body :</span>
+                {{ post.body }}
+                <span class="text-green-500">
+
+                  <p class="font-base">user:{{ post.userId }}</p>
+
+                </span>
+              </h3>
+            </div>
+          </div>
+          <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">edit post</button>
+        </div>
+    </div>
+  </div>
+  </div>
+  </div>
   </div>
 </template>
 <script>
@@ -191,43 +329,44 @@ export default {
         userId: this.post.userId,
       };
       PostDataService.create(data)
-        .then((Response) => {
+        .then(Response => {
           this.post.id = Response.data.id;
+          this.posts.push(Response.data);
           console.log(Response.data.id);
           this.post = {};
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
       this.submitted = true;
     },
     getPost() {
       PostDataService.getAll()
-        .then((response) => {
+        .then(response => {
           this.tenders = response.data;
           console.log(response.data);
           this.posts = response.data;
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
     },
     updatePost() {
       PostDataService.update(this.currentPost.id, this.currentPost)
-        .then((response) => {
+        .then(response => {
           console.log(response.data);
           this.message = "The Tender was updated successfully!";
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
     },
     deletePost() {
       PostDataService.delete(this.currentPost.id)
-        .then((response) => {
+        .then(response => {
           console.log(response.data);
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
     },
@@ -238,11 +377,11 @@ export default {
 
     retrievePosts() {
       PostDataService.getAll()
-        .then((response) => {
+        .then(response => {
           this.posts = response.data;
           console.log(response.data);
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
     },
@@ -264,21 +403,21 @@ export default {
 
     removeAllposts() {
       PostDataService.deleteAll()
-        .then((response) => {
+        .then(response => {
           console.log(response.data);
           this.refreshList();
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
     },
     searchTitle() {
       PostDataService.findByTitle(this.title)
-        .then((response) => {
+        .then(response => {
           this.posts = response.data;
           console.log(response.data);
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
         });
     },
