00ce562fda4c1f4e03f5dfecf79b38ca954a2bd9
rajmohan6268 <48435037+rajmohan6268@users.noreply.github.com>
2020-08-14 03:03:57 +0530

    (feat):add form for editing and arraged posts with serial number

--- pages/try.vue
+++ pages/try.vue
@@ -6,7 +6,7 @@
                     class="block text-gray-700 text-sm font-bold mb-2"
                     for="username"
                 >
-                    Username
+                    text
                     </label>
                     <input
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -15,51 +15,33 @@
                         placeholder="Username"
                     >
             </div>
-            <div class="mb-4">
+            <div class="mb-6">
                 <label
                     class="block text-gray-700 text-sm font-bold mb-2"
-                    for="username"
+                    for="password"
                 >
-                    Username
+                    text
                     </label>
                     <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username"
-                        type="text"
-                        placeholder="Username"
-                    >
-            </div>
-            <div class="mb-4">
-                <label
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    for="username"
-                >
-                    Username
-                    </label>
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username"
-                        type="text"
-                        placeholder="Username"
-                    >
-            </div>
+                        class="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="******************"
+                    > </div>
             <div class="flex items-center justify-between">
                 <button
                     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                     type="button"
                 >
-                    Sign In
+                    text
                     </button>
                     <a
                         class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
                         href="#"
                     >
-                        Forgot Password?
-                        </a>
+                        text </a>
             </div>
         </form>
-        <p class="text-center text-gray-500 text-xs">
-            &copy;2020 Acme Corp. All rights reserved.
-        </p>
+
     </div>
 </template>
